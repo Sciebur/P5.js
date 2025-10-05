45d719c750ab4914e4744d187b868007d8e123f4
Sciebur <mateusz1607@gmail.com>
2025-10-05 18:20:54 +0200

    RandomWalker: 4 space

--- sketches/RandomWalker/sketch.js
+++ sketches/RandomWalker/sketch.js
@@ -1,4 +1,3 @@
-
 let x;
 let y;
 let r;
@@ -9,52 +8,52 @@ let dColor;
 let rot;
 
 function setup() {
-  createCanvas(500, 500);
-  dPos = 3;
-  dColor = 8;
-  frameRate(1000);
-  background(0);
+    createCanvas(500, 500);
+    dPos = 3;
+    dColor = 8;
+    frameRate(1000);
+    background(0);
 
-  x = 0;
-  y = 0;
-  r = 0;
-  g = 0;
-  b = 0;
-  rot = 0;
+    x = 0;
+    y = 0;
+    r = 0;
+    g = 0;
+    b = 0;
+    rot = 0;
 }
 
 function draw() {
-  x = x + random(-dPos, dPos);
-  y = y + random(-dPos, dPos);
+    x = x + random(-dPos, dPos);
+    y = y + random(-dPos, dPos);
 
-  r = r + random(-dColor, dColor);
-  g = g + random(-dColor, dColor);
-  b = b + random(-dColor, dColor);
+    r = r + random(-dColor, dColor);
+    g = g + random(-dColor, dColor);
+    b = b + random(-dColor, dColor);
 
-  rot += 0.0001;
+    rot += 0.0001;
 
-  if (x < -width / 2) x = -width / 2;
-  else if (x > width / 2) x = width / 2;
+    if (x < -width / 2) x = -width / 2;
+    else if (x > width / 2) x = width / 2;
 
-  if (y < -height / 2) y = -height / 2;
-  else if (y > height / 2) y = height / 2;
+    if (y < -height / 2) y = -height / 2;
+    else if (y > height / 2) y = height / 2;
 
-  if (r < 0) r = 0;
-  else if (r > 255) r = 255;
+    if (r < 0) r = 0;
+    else if (r > 255) r = 255;
 
-  if (g < 0) g = 0;
-  else if (g > 255) g = 255;
+    if (g < 0) g = 0;
+    else if (g > 255) g = 255;
 
-  if (b < 0) b = 0;
-  else if (b > 255) b = 255;
+    if (b < 0) b = 0;
+    else if (b > 255) b = 255;
 
-  push();
-  translate(width / 2, height / 2);
-  //rotate(rot);
-  stroke(r, g, b);
-  point(x, y);
-  point(x, -y);
-  point(-x, y);
-  point(-x, -y);
-  pop();
+    push();
+    translate(width / 2, height / 2);
+    //rotate(rot);
+    stroke(r, g, b);
+    point(x, y);
+    point(x, -y);
+    point(-x, y);
+    point(-x, -y);
+    pop();
 }
