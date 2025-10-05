c0fc7da14903f3d9445a66d351f132a9fca5d210
Sciebur <mateusz1607@gmail.com>
2025-10-05 16:55:37 +0200

    Init + crossinglines

---
+++ CrossingLines/sketch.js
@@ -0,0 +1,37 @@
+let lineWidth = 14;
+let lineSpacing = 25;
+let slidingFrames = 100;
+let rotationFrames = 50000000;
+
+let lineLength;
+let count;
+
+function setup() {
+  createCanvas(800, 800);
+  frameRate(50);
+  background(0);
+  colorMode(RGB);
+  
+  count = floor(height / lineWidth);
+  lineLength = height / 3;
+}
+
+function draw() {
+  background(0);
+  stroke(255);
+  strokeWeight(lineWidth);
+  strokeCap(ROUND);
+
+  translate(width / 2, height / 2);
+  rotate(map(frameCount, 0, rotationFrames, 0, TWO_PI));
+
+  let phase = map(frameCount, 0, slidingFrames, 0, TWO_PI);
+
+  for (let i = -count / 2; i < count / 2; i++) {
+    let shift = map(i, 0, count, -PI, PI);
+    let rotation = sin(phase + shift) * (width / 2 - lineLength / 2) - lineLength / 2;
+    
+    line(rotation, i * lineSpacing, rotation + lineLength, i * lineSpacing);
+    line(i * lineSpacing, rotation, i * lineSpacing, rotation + lineLength);
+  }
+}
