52c512c52c98b42b02ccb856f6a826823d972502
Sciebur <mateusz1607@gmail.com>
2025-10-05 18:18:20 +0200

    added Random walker

--- sketches.js
+++ sketches.js
@@ -1,3 +1,4 @@
 const sketches = [
-  { name: "Crossing lines", path: "sketches/CrossingLines" }
+  { name: "Crossing lines", path: "sketches/CrossingLines" },
+  { name: "Random walker", path: "sketches/RandomWalker" }
 ];
