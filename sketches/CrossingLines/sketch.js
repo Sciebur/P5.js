2dcec034d567c1a7e71929599fa94650c9d349e8
Sciebur <mateusz1607@gmail.com>
2025-10-05 17:20:22 +0200

    4space

--- sketches/CrossingLines/sketch.js
+++ sketches/CrossingLines/sketch.js
@@ -7,32 +7,32 @@ let lineLength;
 let count;
 
 function setup() {
-  let CANVAS_SIZE = int(new URLSearchParams(window.location.search).get("size")) || 800;
-  createCanvas(CANVAS_SIZE, CANVAS_SIZE);
-  frameRate(50);
-  background(0);
-  colorMode(RGB);
+    let CANVAS_SIZE = int(new URLSearchParams(window.location.search).get("size")) || 800;
+    createCanvas(CANVAS_SIZE, CANVAS_SIZE);
+    frameRate(50);
+    background(0);
+    colorMode(RGB);
 
-  count = floor(height / lineWidth);
-  lineLength = height / 3;
+    count = floor(height / lineWidth);
+    lineLength = height / 3;
 }
 
 function draw() {
-  background(0);
-  stroke(255);
-  strokeWeight(lineWidth);
-  strokeCap(ROUND);
+    background(0);
+    stroke(255);
+    strokeWeight(lineWidth);
+    strokeCap(ROUND);
 
-  translate(width / 2, height / 2);
-  rotate(map(frameCount, 0, rotationFrames, 0, TWO_PI));
+    translate(width / 2, height / 2);
+    rotate(map(frameCount, 0, rotationFrames, 0, TWO_PI));
 
-  let phase = map(frameCount, 0, slidingFrames, 0, TWO_PI);
+    let phase = map(frameCount, 0, slidingFrames, 0, TWO_PI);
 
-  for (let i = -count / 2; i < count / 2; i++) {
-    let shift = map(i, 0, count, -PI, PI);
-    let rotation = sin(phase + shift) * (width / 2 - lineLength / 2) - lineLength / 2;
+    for (let i = -count / 2; i < count / 2; i++) {
+        let shift = map(i, 0, count, -PI, PI);
+        let rotation = sin(phase + shift) * (width / 2 - lineLength / 2) - lineLength / 2;
 
-    line(rotation, i * lineSpacing, rotation + lineLength, i * lineSpacing);
-    line(i * lineSpacing, rotation, i * lineSpacing, rotation + lineLength);
-  }
+        line(rotation, i * lineSpacing, rotation + lineLength, i * lineSpacing);
+        line(i * lineSpacing, rotation, i * lineSpacing, rotation + lineLength);
+    }
 }
